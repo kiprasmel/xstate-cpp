fa75317368d98ea36ac2957cac881370a91060e5
Kipras Melnikovas <kipras@kipras.org>
2020-01-02 22:51:04 +0200

    Create `xstate.cpp`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ xstate.cpp
@@ -0,0 +1,14 @@
+// xstate.cpp
+
+/**
+ * Originally by Kipras Melnikovas (https://kipras.org) <kipras@kipras.org>
+ * MIT Licensed
+ *
+ *
+ * compile with:
+ *
+ * ```sh
+ * g++ -std=c++11 ./xstate.cpp -o xstate.out
+ * ```
+ *
+*/
