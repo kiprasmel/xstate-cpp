4db50bdc5806ffb0ba227d888434e40cb51c9b11
Kipras Melnikovas <kipras@kipras.org>
2020-01-03 05:08:16 +0200

    Fix `Interpreter` stuff
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- xstate.cpp
+++ xstate.cpp
@@ -74,143 +74,119 @@ const char *StateMachine::transition(const char *currentState, const char *event
 	return nextState;
 }
 
-struct Interpreter {
-	private:
+Interpreter::Interpreter(StateMachine *stateMachine)
+	:
+	stateMachine(stateMachine),
+	state(new InterpreterState({ .value = stateMachine->initial})),
+	status(notStarted)
+{
+}
 
-	InterpreterStatus status;
+const char *Interpreter::getStatusStr() const {
+	return InterpreterStatusStrings[this->status];
+}
 
-	InterpreterState *state;
-	StateMachine *stateMachine;
+Interpreter *Interpreter::logInfo() {
+	printf("status = %d (%s) | state = %s \n", this->status, this->getStatusStr(), this->state->value);
+	return this;
+}
 
-	public:
+Interpreter *Interpreter::send(const char *event) {
+	if (this->status != started) {
+		fprintf(
+			stderr,
+			"\nERR interpreter.send was called when it was not started / stopped (%s).\n",
+			this->getStatusStr()
+		);
 
-	Interpreter(StateMachine *stateMachine)
-		:
-		stateMachine(stateMachine),
-		state(new InterpreterState({ .value = stateMachine->initial})),
-		status(notStarted)
-	{
+		throw;
 	}
 
-	const char *getStatusStr() const {
-		return InterpreterStatusStrings[this->status];
+	const char *nextState = this->stateMachine->transition(this->state->value, event);
+	// printf("nextState %s\n", nextState);
+
+	/**
+		* An event might've been fired at the wrong time /
+		* when the state didn't have any handlers for that specific event,
+		* thus we just skip it, since that's allowed.
+		*/
+	if (nextState != NULL) {
+		this->state->value = nextState;
 	}
 
-	Interpreter *logInfo() {
-		printf("status = %d (%s) | state = %s \n", this->status, this->getStatusStr(), this->state->value);
-		return this;
-	}
+	this->handleOnTransition();
 
-	Interpreter *send(const char *event) {
-		if (this->status != started) {
-			fprintf(
-				stderr,
-				"\nERR interpreter.send was called when it was not started / stopped (%s).\n",
-				this->getStatusStr()
-			);
+	return this;
+}
 
-			throw;
-		}
-
-		const char *nextState = this->stateMachine->transition(this->state->value, event);
-		// printf("nextState %s\n", nextState);
-
-		/**
-		 * An event might've been fired at the wrong time /
-		 * when the state didn't have any handlers for that specific event,
-		 * thus we just skip it, since that's allowed.
-		 */
-		if (nextState != NULL) {
-			this->state->value = nextState;
-		}
-
-		this->handleOnTransition();
-
-		return this;
-	}
-
-	/** handlers (identical) */
+/** handlers (identical) */
 
 
 
-	// void onStart(std::function<void(const InterpreterState *state)> callback = [](const InterpreterState *state) {}) const {
-	// 	callback(this->state);
-	// }
+// void onStart(std::function<void(const InterpreterState *state)> callback = [](const InterpreterState *state) {}) const {
+// 	callback(this->state);
+// }
 
-	Interpreter *start() {
-		this->status = started;
-		this->handleOnStart();
+Interpreter *Interpreter::start() {
+	this->status = started;
+	this->handleOnStart();
 
-		return this;
-	}
+	return this;
+}
 
-	/** the one we call ourselves */
-	private: std::function<const void()> handleOnStart = []() {};
 
-	public:
-	Interpreter *onStart(const std::function<const void(                 )> callback = [](                 ) {}) {
-		this->handleOnStart = [&]() { callback(    ); };
-		return this;
-	}
+Interpreter *Interpreter::onStart(const std::function<const void(                 )> callback = [](                 ) {}) {
+	this->handleOnStart = [&]() { callback(    ); };
+	return this;
+}
 
-	/** the one we expose to the consumer so he can inject the callback */
-	Interpreter *onStart(const std::function<const void(Interpreter *self)> callback = [](Interpreter *self) {}) {
-		this->handleOnStart = [&]() { callback(this); };
-		return this;
-	}
+/** the one we expose to the consumer so he can inject the callback */
+Interpreter *Interpreter::onStart(const std::function<const void(Interpreter *self)> callback = [](Interpreter *self) {}) {
+	this->handleOnStart = [&]() { callback(this); };
+	return this;
+}
 
 
 
-	// std::function<void(                             )> onTransition = [](                             ) {};
-	// std::function<void(const InterpreterState *state)> onTransition = [](const InterpreterState *state) {};
+// std::function<void(                             )> onTransition = [](                             ) {};
+// std::function<void(const InterpreterState *state)> onTransition = [](const InterpreterState *state) {};
 
-	// void onTransition(std::function<void()> callback = []() {}) const {
-	// 	callback();
-	// }
+// void onTransition(std::function<void()> callback = []() {}) const {
+// 	callback();
+// }
 
-	/** the one we call ourselves */
-	private: std::function<const void()> handleOnTransition = []() {};
+Interpreter *Interpreter::onTransition(const std::function<const void(                 )> callback = [](                 ) {}) {
+	this->handleOnTransition = [&]() { callback(    ); };
+	return this;
+}
 
-	/** the one we expose to the consumer so he can inject the callback */
-	public:
-	Interpreter *onTransition(const std::function<const void(                 )> callback = [](                 ) {}) {
-		this->handleOnTransition = [&]() { callback(    ); };
-		return this;
-	}
-
-	Interpreter *onTransition(const std::function<const void(Interpreter *self)> callback = [](Interpreter *self) {}) {
-		this->handleOnTransition = [&]() { callback(this); };
-		return this;
-	}
+Interpreter *Interpreter::onTransition(const std::function<const void(Interpreter *self)> callback = [](Interpreter *self) {}) {
+	this->handleOnTransition = [&]() { callback(this); };
+	return this;
+}
 
 
 
-	// std::function<void()> onStop = []() {};
-	// void onStop(std::function<void(const InterpreterState *state)> callback = [](const InterpreterState *state) {}) const {
-	// 	callback(this->state);
-	// }
+// std::function<void()> onStop = []() {};
+// void onStop(std::function<void(const InterpreterState *state)> callback = [](const InterpreterState *state) {}) const {
+// 	callback(this->state);
+// }
 
-	Interpreter *stop() {
-		this->status = stopped;
-		this->handleOnStop();
-		return this;
-	}
+Interpreter *Interpreter::stop() {
+	this->status = stopped;
+	this->handleOnStop();
+	return this;
+}
 
-	/** the one we call ourselves */
-	private: std::function<const void()> handleOnStop = []() {};
+Interpreter *Interpreter::onStop(const std::function<const void(                 )> callback = [](                 ) {}) {
+	this->handleOnStop = [&]() { callback(    ); };
+	return this;
+}
 
-	/** the one we expose to the consumer so he can inject the callback */
-	public:
-	Interpreter *onStop(const std::function<const void(                 )> callback = [](                 ) {}) {
-		this->handleOnStop = [&]() { callback(    ); };
-		return this;
-	}
-
-	Interpreter *onStop(const std::function<const void(Interpreter *self)> callback = [](Interpreter *self) {}) {
-		this->handleOnStop = [&]() { callback(this); };
-		return this;
-	}
-};
+Interpreter *Interpreter::onStop(const std::function<const void(Interpreter *self)> callback = [](Interpreter *self) {}) {
+	this->handleOnStop = [&]() { callback(this); };
+	return this;
+}
 
 Interpreter *interpret(StateMachine  stateMachine) {
 	Interpreter *interpreter = new Interpreter(new StateMachine(stateMachine));
@@ -226,70 +202,70 @@ Interpreter *interpret(StateMachine *stateMachine) {
 
 } // namespace xs
 
-int main() {
-	xs::StateMachine machine = {
-		.id = "light",
-		.initial = "green",
-		.states = {
-			{
-				"green" ,
-				{ .on = { { "TIMER", "yellow" } } }
-			},
-			{
-				"yellow",
-				{ .on = { { "TIMER", "red"    } } }
-			},
-			{
-				"red"  ,
-				{
-					.on = { { "TIMER", "red.red-100"  } },
-					.nested = {
-						.id = "red-brightness",
-						.initial = "red-100",
-						.states = {
-							{
-								"red-100",
-								{ .on = { { "TIMER", "red.red-0" } } }
-							},
-							{
-								"red-0",
-								{ .on = { { "TIMER", "green" } } }
-							}
-						}
-					}
-				},
-			}
-		}
-	};
+// int main() {
+// 	xs::StateMachine machine = {
+// 		.id = "light",
+// 		.initial = "green",
+// 		.states = {
+// 			{
+// 				"green" ,
+// 				{ .on = { { "TIMER", "yellow" } } }
+// 			},
+// 			{
+// 				"yellow",
+// 				{ .on = { { "TIMER", "red"    } } }
+// 			},
+// 			{
+// 				"red"  ,
+// 				{
+// 					.on = { { "TIMER", "red.red-100"  } },
+// 					.nested = {
+// 						.id = "red-brightness",
+// 						.initial = "red-100",
+// 						.states = {
+// 							{
+// 								"red-100",
+// 								{ .on = { { "TIMER", "red.red-0" } } }
+// 							},
+// 							{
+// 								"red-0",
+// 								{ .on = { { "TIMER", "green" } } }
+// 							}
+// 						}
+// 					}
+// 				},
+// 			}
+// 		}
+// 	};
 
-	xs::Interpreter *toggleMachine = xs::interpret(machine)
-		->logInfo()
-		->onStart([]() {
-			printf("let's go!\n");
-		})
-		->onTransition([](xs::Interpreter *self) {
-			self->logInfo();
-		})
-		->onStop([]() {
-			printf("oh no we stopped c:\n");
-		})
-		->start();
+// 	xs::Interpreter *toggleMachine = xs::interpret(machine)
+// 		->logInfo()
+// 		->onStart([]() {
+// 			printf("let's go!\n");
+// 		})
+// 		->onTransition([](xs::Interpreter *self) {
+// 			self->logInfo();
+// 		})
+// 		->onStop([]() {
+// 			printf("oh no we stopped c:\n");
+// 		})
+// 		->start();
 
-	toggleMachine->send("TIMER");
+// 	toggleMachine->send("TIMER");
 
-	toggleMachine->send("TIMER");
+// 	toggleMachine->send("TIMER");
 
-	toggleMachine->send("TIMER");
+// 	toggleMachine->send("TIMER");
 
-	toggleMachine->send("TIMER");
+// 	toggleMachine->send("TIMER");
 
-	toggleMachine->send("TIMER");
+// 	toggleMachine->send("TIMER");
 
-	toggleMachine->send("TIMER");
+// 	toggleMachine->send("TIMER");
 
-	toggleMachine->stop();
+// 	toggleMachine->stop();
 
-	delete toggleMachine;
+// 	delete toggleMachine;
 
-	return 0;
-}
+// 	return 0;
+// }
--- xstate.h
+++ xstate.h
@@ -63,41 +63,41 @@ struct InterpreterState {
 	const char *value;
 };
 
-// struct Interpreter {
-// 	private:
-// 	InterpreterStatus status      ;
+struct Interpreter {
+	private:
+	InterpreterStatus status      ;
 
-// 	InterpreterState *state       ;
-// 	StateMachine     *stateMachine;
+	InterpreterState *state       ;
+	StateMachine     *stateMachine;
 
-// 	public:
-// 	Interpreter(StateMachine *stateMachine);
+	public:
+	Interpreter(StateMachine *stateMachine);
 
-// 	const char  *getStatusStr() const;
-// 	Interpreter *logInfo     ()      ;
+	const char  *getStatusStr() const;
+	Interpreter *logInfo     ()      ;
 
-// 	/** handlers */
-// 	Interpreter *start(                 );
-// 	Interpreter *send (const char *event);
-// 	Interpreter *stop (                 );
+	/** handlers */
+	Interpreter *start(                 );
+	Interpreter *send (const char *event);
+	Interpreter *stop (                 );
 
-// 	private:
-// 	std::function<const void()> handleOnStart     ;
-// 	std::function<const void()> handleOnTransition;
-// 	std::function<const void()> handleOnStop      ;
+	private:
+	std::function<const void()> handleOnStart     ;
+	std::function<const void()> handleOnTransition;
+	std::function<const void()> handleOnStop      ;
 
-// 	public:
-// 	Interpreter *onStart(     const std::function<const void(                 )> callback = [](                 ) {});
-// 	Interpreter *onStart(     const std::function<const void(Interpreter *self)> callback = [](Interpreter *self) {});
+	public:
+	Interpreter *onStart     (const std::function<const void(                 )> callback);
+	Interpreter *onStart     (const std::function<const void(Interpreter *self)> callback);
 
-// 	Interpreter *onTransition(const std::function<const void(                 )> callback = [](                 ) {});
-// 	Interpreter *onTransition(const std::function<const void(Interpreter *self)> callback = [](Interpreter *self) {});
+	Interpreter *onTransition(const std::function<const void(                 )> callback);
+	Interpreter *onTransition(const std::function<const void(Interpreter *self)> callback);
 
-// 	Interpreter *onStop(      const std::function<const void(                 )> callback = [](                 ) {});
-// 	Interpreter *onStop(      const std::function<const void(Interpreter *self)> callback = [](Interpreter *self) {});
-// };
+	Interpreter *onStop      (const std::function<const void(                 )> callback);
+	Interpreter *onStop      (const std::function<const void(Interpreter *self)> callback);
+};
 
-// Interpreter *interpret(StateMachine  stateMachine);
-// Interpreter *interpret(StateMachine *stateMachine);
+Interpreter *interpret(StateMachine  stateMachine);
+Interpreter *interpret(StateMachine *stateMachine);
 
 } // namespace xs
