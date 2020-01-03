2d35a08e2ce861956664217c8fe8ae023e2e492d
Kipras Melnikovas <kipras@kipras.org>
2020-01-03 04:31:12 +0200

    Create `splitStr` util to make nesting possible 👀👀
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- xstate.cpp
+++ xstate.cpp
@@ -19,7 +19,24 @@
 
 #include "xstate.h"
 
-// using namespace xs;
+#include <vector>
+#include <string>
+#include <sstream>
+
+std::vector<std::string> splitStr(std::string str = "", const char delim = ' ') {
+    std::string buf;                 // Have a buffer string
+    std::stringstream ss(str);       // Insert the string into a stream
+
+    std::vector<std::string> tokens; // Create vector to hold our words
+
+    while (getline(ss, buf, delim)) {
+        tokens.push_back(buf);
+	}
+
+	return tokens;
+}
+
+namespace xs {
 
 int main() {
 	xs::StateMachine machine = {
